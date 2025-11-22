253269b38deb3e2182a47419ddc6d27dab02627b
Ryusatoru <Choleros25@users.noreply.github.com>
2025-11-22 13:23:12 -0600

    fix path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/portafolio/" : "/", 
-  plugins: [
-    tailwindcss(),
-  ],
+  base: mode === "production" ? "/portafolio/" : "/",
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "pages/aboutme.html"),
+      },
+    },
+  },
 }));
